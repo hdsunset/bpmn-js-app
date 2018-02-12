e9ce770083690a150b66db373c4d6a3d1c90de44
hdsunset <web.hawker@mail.ru>
2018-02-12 23:13:29 +0300

    still not

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
 const express = require('express');
 var app = express();
+
+const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+
 app.use(express.static('public'));
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://lokineji:sunset777@ds229008.mlab.com:29008/bpmn_database";
-
-$("#js-export-diagram").click((e)=>{
-  console.log('Zdarova');
-  saveDiagramToBD(function(err, xml) {
-    
-     //setEncode(exportLink, 'diagram.bpmn', err ? null : xml);
-  
-      MongoClient.connect(url, function(err,db) {
-        var collection = db.collection('bpmn-diagrams');      
-        collection.insertOne(xml, function(err,result){
-   
-          if(err) {
-            console.log(err);
-            return;
-          }
-          console.log(result.ops);
-          db.close();
-        });
-      });
-   
-  });
-
 app.get("/", (req, res) => {
-  res.sendFile("./index.html");
-  console.log('Hello')
+  res.sendFile("./index.html")
 });
 
+var url ="mongodb://lokineji:sunset777@ds229008.mlab.com:29008/bpmn_database";
+/// MongoDB
+
+/// catch ajax requests
+///GET
+app.get("/exporttodb", (req, res) => {
+  var keyData = [];
+  db.createKeyStream()
+  .on('data', function (data) {
+  keyData.push(data.toString())
+  })
+  .on('end', function () {
+  res.end(JSON.stringify({keys: keyData}))
+  });
+  ///Insert to Mongodb
+  MongoClient.connect(url, function(err,db) {
+    var collection = db.collection('bpmn-diagrams');      
+    collection.insertOne(req.body, function(err,result){
+ 
+      if(err) {
+        console.log(err);
+        return;
+      }
+      console.log(result.ops);
+      db.close();
+
+     });
+   });
+
+
+  });
+
+  
+///
+// app.get("/exporttodb", (req, res) => {
+//   db.get(req.query.name, function (err, value) {
+//     if (err) return res.status(204).send("NO FILE")
+//     res.send(value);
+//     });
+//     });
+
+  
 app.listen(3000);
--- init.js
+++ init.js
@@ -44,9 +44,8 @@ window.bpmnjs = viewer;*/
 
 
 
-
-var $ = require('jquery');
-BpmnModeler = require('bpmn-js/lib/Modeler');
+var $ = require('jquery'),
+    BpmnModeler = require('bpmn-js/lib/Modeler');
 
 var container = $('#js-drop-zone');
 
@@ -55,99 +54,92 @@ var canvas = $('#js-canvas');
 var modeler = new BpmnModeler({ container: canvas });
 
 var newDiagramXML = '<?xml version="1.0" encoding="UTF-8"?>' +
-'<bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-                'xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" ' +
-                'xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" ' +
-                'xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" ' +
-                'targetNamespace="http://bpmn.io/schema/bpmn" ' +
-                'id="Definitions_1">' +
-'<bpmn:process id="Process_1" isExecutable="false">' +
-  '<bpmn:startEvent id="StartEvent_1"/>' +
-'</bpmn:process>' +
-'<bpmndi:BPMNDiagram id="BPMNDiagram_1">' +
-  '<bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">' +
-    '<bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">' +
-      '<dc:Bounds height="36.0" width="36.0" x="173.0" y="102.0"/>' +
-    '</bpmndi:BPMNShape>' +
-  '</bpmndi:BPMNPlane>' +
-'</bpmndi:BPMNDiagram>' +
-'</bpmn:definitions>';
+  '<bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+                    'xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" ' +
+                    'xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" ' +
+                    'xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" ' +
+                    'targetNamespace="http://bpmn.io/schema/bpmn" ' +
+                    'id="Definitions_1">' +
+    '<bpmn:process id="Process_1" isExecutable="false">' +
+      '<bpmn:startEvent id="StartEvent_1"/>' +
+    '</bpmn:process>' +
+    '<bpmndi:BPMNDiagram id="BPMNDiagram_1">' +
+      '<bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">' +
+        '<bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">' +
+          '<dc:Bounds height="36.0" width="36.0" x="173.0" y="102.0"/>' +
+        '</bpmndi:BPMNShape>' +
+      '</bpmndi:BPMNPlane>' +
+    '</bpmndi:BPMNDiagram>' +
+  '</bpmn:definitions>';
 
 function createNewDiagram() {
-openDiagram(newDiagramXML);
+  openDiagram(newDiagramXML);
 }
 
 function openDiagram(xml) {
 
-modeler.importXML(xml, function(err) {
+  modeler.importXML(xml, function(err) {
 
-if (err) {
-  container
-    .removeClass('with-diagram')
-    .addClass('with-error');
+    if (err) {
+      container
+        .removeClass('with-diagram')
+        .addClass('with-error');
 
-  container.find('.error pre').text(err.message);
+      container.find('.error pre').text(err.message);
 
-  console.error(err);
-} else {
-  container
-    .removeClass('with-error')
-    .addClass('with-diagram');
+      console.error(err);
+    } else {
+      container
+        .removeClass('with-error')
+        .addClass('with-diagram');
+    }
+
+
+  });
 }
 
-
-});
-}
-/////// Функции кнопок импорта и экспорта - to XML////////////////////////////////////
 function saveSVG(done) {
-modeler.saveSVG(done);
+  modeler.saveSVG(done);
 }
 
 function saveDiagram(done) {
 
-modeler.saveXML({ format: true }, function(err, xml) {
-done(err, xml);
-});
-}
-
-function saveDiagramToDB(done) {
-
-modeler.saveXML({ format: true }, function(err, xml) {
-  done(err, xml);
-});
+  modeler.saveXML({ format: true }, function(err, xml) {
+    done(err, xml);
+  });
 }
 
 function registerFileDrop(container, callback) {
 
-function handleFileSelect(e) {
-e.stopPropagation();
-e.preventDefault();
+  function handleFileSelect(e) {
+    e.stopPropagation();
+    e.preventDefault();
 
-var files = e.dataTransfer.files;
+    var files = e.dataTransfer.files;
 
-var file = files[0];
+    var file = files[0];
 
-var reader = new FileReader();
+    var reader = new FileReader();
 
-reader.onload = function(e) {
+    reader.onload = function(e) {
 
-  var xml = e.target.result;
+      var xml = e.target.result;
 
-  callback(xml);
-};
+      callback(xml);
+    };
 
-reader.readAsText(file);
-}
+    reader.readAsText(file);
+  }
 
-function handleDragOver(e) {
-e.stopPropagation();
-e.preventDefault();
+  function handleDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
 
-e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-}
+    e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
 
-container.get(0).addEventListener('dragover', handleDragOver, false);
-container.get(0).addEventListener('drop', handleFileSelect, false);
+  container.get(0).addEventListener('dragover', handleDragOver, false);
+  container.get(0).addEventListener('drop', handleFileSelect, false);
 }
 
 
@@ -155,99 +147,100 @@ container.get(0).addEventListener('drop', handleFileSelect, false);
 
 // check file api availability
 if (!window.FileList || !window.FileReader) {
-window.alert(
-'Looks like you use an older browser that does not support drag and drop. ' +
-'Try using Chrome, Firefox or the Internet Explorer > 10.');
+  window.alert(
+    'Looks like you use an older browser that does not support drag and drop. ' +
+    'Try using Chrome, Firefox or the Internet Explorer > 10.');
 } else {
-registerFileDrop(container, openDiagram);
+  registerFileDrop(container, openDiagram);
 }
 
 // bootstrap diagram functions
 
 $(function() {
 
-$('#js-create-diagram').click(function(e) {
-e.stopPropagation();
-e.preventDefault();
+  $('#js-create-diagram').click(function(e) {
+    e.stopPropagation();
+    e.preventDefault();
 
-createNewDiagram();
-});
-// ПЕРЕМЕННЫЕ КНОПОК
-var downloadLink = $('#js-download-diagram');
-var downloadSvgLink = $('#js-download-svg');
-var exportLink = $('#js-export-diagram');
-console.log($("#js-export-diagram"));
-////////////////////////////////////////////////SAVE TO DB
-
- 
-  
-  // });
-//});
-});
-//$("#js-export-diagram").click(function(e){
-
-
-  
-
-
-
-
-$('.buttons a').click(function(e) {
-if (!$(this).is('.active')) {
-  e.preventDefault();
-  e.stopPropagation();
-}
-});
-
-
-////////////////////////МЕНЯТЬ ЭТОТ МЕТОД///////////////////////////////
-function setEncoded(link, name, data) {
-var encodedData = encodeURIComponent(data);
-
-if (data) {
-  link.addClass('active').attr({
-    'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
-    'download': name
+    createNewDiagram();
   });
-} else {
-  link.removeClass('active');
-}
-};
-////ФУНКЦИИ ВЫГРУЗКИ ДИАГРАММ В БД ---НИЧЕГО НЕ МЕНЯТЬ ??SETENCODED
-var _ = require('lodash');
 
-var exportArtifacts = _.debounce(function() {
-
-saveSVG(function(err, svg) {
-  setEncoded(downloadSvgLink, 'diagram.svg', err ? null : svg);
-});
-
-saveDiagram(function(err, xml) {
-  setEncoded(downloadLink, 'diagram.bpmn', err ? null : xml);
+  var downloadLink = $('#js-download-diagram');
+  var downloadSvgLink = $('#js-download-svg');
 
 
-//  saveDiagramToBD(function(err, xml) {
+  $('.buttons a').click(function(e) {
+    if (!$(this).is('.active')) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+
+
+  $('#js-export-diagram').click(function(e) {
+    $.ajax({
+      url: "/exporttodb",
+      type: "POST",
+      dataType: "json",
+      success: (data) => {
+      keyList = data.keys;
+      updateSearchList(keyList);
+      }
+      })
+      
+  });
+
+  $('#js-import-diagram').click(function(e) {
+    //  e.stopPropagation()
+     // e.preventDefault();
+     // if (e.path[0].className !== "search-list-elem") return;
+      $.ajax({
+      url: "/exporttodb",
+      type: "GET",
+      data: {name: e.path[0].innerText},
+      dataType:'text',
+      success: (data) => {
+      openDiagram(data);
+      $("#input-diagram-name")[0].value = e.path[0].innerText
+      $("#diagram-name")[0].innerHTML = `Current diagram: <b>${e.path[0].innerText}</b>`
+      },
+      error: (err) => {
+      console.log("err",err);
+      if (err.status != 200) {
+      alert('Diagram doesn\'t exists');
+      } else {
+      openDiagram(err.responseText);
+      }
+      }
+      })
+      })
   
-//   //setEncode(exportLink, 'diagram.bpmn', err ? null : xml);
 
-//    MongoClient.connect(url, function(err,db) {
-//      var collection = db.collection('bpmn-diagrams');      
-//      collection.insertOne(xml, function(err,result){
- 
-//        if(err) {
-//          console.log(err);
-//          return;
-//        }
-//        console.log(result.ops);
-//        db.close();
-//      });
-//    });
+  function setEncoded(link, name, data) {
+    var encodedData = encodeURIComponent(data);
 
-//  });
+    if (data) {
+      link.addClass('active').attr({
+        'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
+        'download': name
+      });
+    } else {
+      link.removeClass('active');
+    }
+  }
 
-}, 500);
+  var _ = require('lodash');
 
+  var exportArtifacts = _.debounce(function() {
 
+    saveSVG(function(err, svg) {
+      setEncoded(downloadSvgLink, 'diagram.svg', err ? null : svg);
+    });
 
-modeler.on('commandStack.changed', exportArtifacts);
+    saveDiagram(function(err, xml) {
+      setEncoded(downloadLink, 'diagram.bpmn', err ? null : xml);
+    });
+  }, 500);
+
+  modeler.on('commandStack.changed', exportArtifacts);
 });
